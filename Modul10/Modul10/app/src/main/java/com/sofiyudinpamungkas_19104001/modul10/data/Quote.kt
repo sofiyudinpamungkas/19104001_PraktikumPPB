755e684481fd8fe261e3a9ac8ce2bb2545efe8bd
sofiyudinpamungkas <82369655+sofiyudinpamungkas@users.noreply.github.com>
2022-01-23 21:19:34 +0700

    Add files via upload

---
+++ Modul10/Modul10/app/src/main/java/com/sofiyudinpamungkas_19104001/modul10/data/Quote.kt
@@ -0,0 +1,12 @@
+package com.sofiyudinpamungkas_19104001.modul10.data
+
+import android.os.Parcelable
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
